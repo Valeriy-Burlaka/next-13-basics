f3c2dd0073b31f1dcabbf2e706cf8223f3d409f6
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-09-08 15:13:06 +0200

    more stylish styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
             <Link href="/">Home</Link>
             <Link href="/notes">Notes</Link>
           </nav>
+          {children}
         </main>
-        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home Page</h1>
-    </main>
+    </div>
   );
 }
